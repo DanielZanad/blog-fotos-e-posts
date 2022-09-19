3271794e3169d755571103e6281f09c167e07135
DanielZanad <daniel.zanad.dz@gmail.com>
2022-09-19 14:28:30 -0300

    Adiciona rotas para fotos

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,12 +1,14 @@
+import { Post, Photo } from '@prisma/client';
 import { Router } from 'express';
 import { prisma } from '../prisma/prisma';
 
 export const routes = Router();
 
+// Posts
 routes.post('/posts', async (req, res) => {
   const { title, body } = req.body;
 
-  const result = await prisma.post.create({
+  const result: Post = await prisma.post.create({
     data: {
       title,
       body,
@@ -17,7 +19,7 @@ routes.post('/posts', async (req, res) => {
 });
 
 routes.get('/posts', async (req, res) => {
-  const result = await prisma.post.findMany();
+  const result: Array<Post> = await prisma.post.findMany();
 
   return res.status(200).json({ data: result });
 });
@@ -25,7 +27,7 @@ routes.get('/posts', async (req, res) => {
 routes.get('/posts/:id', async (req, res) => {
   const { id } = req.params;
 
-  const result = await prisma.post.findUniqueOrThrow({
+  const result: Post = await prisma.post.findUniqueOrThrow({
     where: {
       id,
     },
@@ -38,7 +40,7 @@ routes.put('/posts/:id', async (req, res) => {
   const { id } = req.params;
   const { title, body } = req.body;
 
-  const result = await prisma.post.update({
+  const result: Post = await prisma.post.update({
     where: {
       id,
     },
@@ -53,7 +55,70 @@ routes.put('/posts/:id', async (req, res) => {
 
 routes.delete('/posts/:id', async (req, res) => {
   const { id } = req.params;
-  const result = await prisma.post.delete({
+  const result: Post = await prisma.post.delete({
+    where: {
+      id,
+    },
+  });
+
+  return res.status(200).json({ data: result });
+});
+
+// Photos
+
+routes.post('/photos', async (req, res) => {
+  const { title, url, thumbnail_url } = req.body;
+
+  const result: Photo = await prisma.photo.create({
+    data: {
+      title,
+      url,
+      thumbnail_url,
+    },
+  });
+
+  return res.status(201).json({ data: result });
+});
+
+routes.get('/photos', async (req, res) => {
+  const result: Array<Photo> = await prisma.photo.findMany();
+
+  return res.status(200).json({ data: result });
+});
+
+routes.get('/photos/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const result: Photo = await prisma.photo.findUniqueOrThrow({
+    where: {
+      id,
+    },
+  });
+
+  return res.status(200).json({ data: result });
+});
+
+routes.put('/photos/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, url, thumbnail_url } = req.body;
+
+  const result: Photo = await prisma.photo.update({
+    where: {
+      id,
+    },
+    data: {
+      title,
+      url,
+      thumbnail_url,
+    },
+  });
+
+  return res.status(200).json({ data: result });
+});
+
+routes.delete('/photos/:id', async (req, res) => {
+  const { id } = req.params;
+  const result: Photo = await prisma.photo.delete({
     where: {
       id,
     },
