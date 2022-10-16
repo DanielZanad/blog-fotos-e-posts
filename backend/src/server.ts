490777485c5924cb6432530fb37013daa12b256e
sam sepiol <samsepi0l@DESKTOP-UVN6IP3.localdomain>
2022-10-16 14:21:00 -0300

    adiciona cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
+import cors from 'cors';
 import express from 'express';
 import { routes } from './routes/routes';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
